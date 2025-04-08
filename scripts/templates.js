89d5ee553bef200501848677d84bd483dcf079a8
VadimMichel <vadimmichel5@gmail.com>
2025-04-08 16:38:41 +0200

    change background color by pokemon type

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,11 @@
 function getPokemonCardTemplate(indexpokemonArray){
     return `
-        <div class="pokemon-container">
+        <div class="pokemon-container ${pokemonArray[indexpokemonArray].types[0].type.name}">
             <div class="pokemon-container-header-footer">
                 <span>#${indexpokemonArray +1}</span><span>${pokemonArray[indexpokemonArray].name}</span>
             </div>
-            <img src="${pokemonArray[indexpokemonArray].sprites.front_default}" alt="pokemon ${pokemonArray[indexpokemonArray].name}">
-            <div class="pokemon-container-header-footer">
-                <img src="" alt="type">
+            <div class="img-wraper"><img src="${pokemonArray[indexpokemonArray].sprites.front_default}" alt="pokemon ${pokemonArray[indexpokemonArray].name}"></div>
+            <div id="pokemonContainerFooter${indexpokemonArray}" class="pokemon-container-header-footer">
             </div>
         </div>
     `
